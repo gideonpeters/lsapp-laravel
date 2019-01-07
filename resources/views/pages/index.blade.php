eb912f9b1a7516fb9dc179b82aea4e8398648737
Gideon Peters <gideonpeters85@gmail.com>
2019-01-08 00:03:31 +0100

    worked on navbar and templating

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,11 +1,15 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-    </head>
-    <body>
-    </body>
-</html>
+@section('content')
+<div class="jumbotron col-xs-1 text-center">
+        <div class="container">
+            <h1>{{$title}}</h1>
+            <p>This is the laravel application from the "Laravel From Scratch" Youtube series</p>
+          <p>
+              <a class="btn btn-primary btn-lg" href="/login" role="button">Login &raquo;</a>
+              <a class="btn btn-success btn-lg" href="/register" role="button">Register &raquo;</a>
+            </p>
+        </div>
+      </div>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@
 
 Route::get('/', 'PagesController@index');
 
-Route::get('/about', function () {
-    return view('pages.about');
-});
+Route::get('/about', 'PagesController@about');
+
+Route::get('/services', 'PagesController@services');
