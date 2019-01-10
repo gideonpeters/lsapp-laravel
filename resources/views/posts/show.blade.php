25c778b8d346f77efcc80b00b0b86531a10ebd8e
Gideon Peters <gideonpeters85@gmail.com>
2019-01-10 09:48:07 +0100

    added edit button

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
 <a href="/posts" class="btn btn-dark m-1">Go Back</a>
-<h1> {{$post->title}}</h1>
+<h1 class="mt-4"> {{$post->title}}</h1>
 <div>
     {!!$post->body!!}
 </div>
 <hr>
 <small>Written on {{$post->created_at}}</small>
+<hr>
+<a href="/posts/{{$post->id}}/edit" class="btn btn-dark">Edit</a>
 
 @endsection
