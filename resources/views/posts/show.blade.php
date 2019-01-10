8187fb6b8b6e2ec7439c74c3eec408871cd8c9c3
Gideon Peters <gideonpeters85@gmail.com>
2019-01-10 12:42:51 +0100

    added user authentication using artisan

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,7 +11,7 @@
 <hr>
 <a href="/posts/{{$post->id}}/edit" class="btn btn-dark">Edit</a>
 
-{!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+{!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
     {{Form::hidden('_method', 'DELETE')}}
     {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
 {!!Form::close()!!}
