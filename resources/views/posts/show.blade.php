05c3b04d0553967898c700606e1284851555cb0c
Gideon Peters <gideonpeters85@gmail.com>
2019-01-11 08:45:51 +0100

    finished up

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,18 +2,23 @@
 
 @section('content')
 <a href="/posts" class="btn btn-dark m-1">Go Back</a>
-<h1 class="mt-4"> {{$post->title}}</h1>
-<div>
-    {!!$post->body!!}
-</div>
-<hr>
-<small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
-<hr>
-<a href="/posts/{{$post->id}}/edit" class="btn btn-dark">Edit</a>
+    <h1 class="mt-4"> {{$post->title}}</h1>
+    <img style="width:100%" src="/storage/cover_images/{{$post->cover_image}}" alt="">
+    <br><br>
+        <div>
+            {!!$post->body!!}
+        </div>
+    <hr>
+        <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
+    <hr>
+    @if (!Auth::guest())
+    @if (Auth::user()->id == $post->user_id)
+        <a href="/posts/{{$post->id}}/edit" class="btn btn-dark">Edit</a>
 
-{!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
-    {{Form::hidden('_method', 'DELETE')}}
-    {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-{!!Form::close()!!}
-
+        {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+            {{Form::hidden('_method', 'DELETE')}}
+            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+        {!!Form::close()!!}
+    @endif
+    @endif
 @endsection
